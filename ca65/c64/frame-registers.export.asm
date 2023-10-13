9ce5de09b0dcee3aeb36b218f4d84ff1b7c8ef60
c64pectre <113802295+c64pectre@users.noreply.github.com>
2023-10-13 20:31:12 +0200

    Registers and frame registers completed / improved macro readability

--- ca65/c64/frame-registers.export.asm
+++ ca65/c64/frame-registers.export.asm
@@ -4,3 +4,9 @@
 .exportzp MPX := $8E
 .exportzp MPL := MPX + 0
 .exportzp MPH := MPX + 1
+
+.exportzp FS_BOTTOM := $14
+.exportzp FS_TOP := $2A
+
+.exportzp RS_BOTTOM := $4B
+.exportzp RS_TOP := $72
